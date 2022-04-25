d55007e8ecc5ead2ae853c3ea4032d89aaef364c
DJUM <djum25@gmail.com>
2022-04-25 11:32:09 +0000

    Create repository secondsary compte

---
+++ src/main/java/com/projet/ferme/repository/SecondaryCompteRepository.java
@@ -0,0 +1,9 @@
+package com.projet.ferme.repository;
+
+import com.projet.ferme.entity.SecondaryCompte;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SecondaryCompteRepository extends JpaRepository<SecondaryCompte, Long>{
+    
+}
