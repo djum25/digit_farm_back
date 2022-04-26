b5f02078fe09300f8c2015a34afb44dbb736c282
DJUM <djum25@gmail.com>
2022-04-26 10:36:17 +0000

    add method to find by secondary compte

--- src/main/java/com/projet/ferme/repository/OperationRepository.java
+++ src/main/java/com/projet/ferme/repository/OperationRepository.java
@@ -1,9 +1,13 @@
 package com.projet.ferme.repository;
 
+import java.util.List;
+
 import com.projet.ferme.entity.Operation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OperationRepository extends JpaRepository<Operation,Long>{
+
+    List<Operation> findBySecondary_id(Long id);
     
 }
