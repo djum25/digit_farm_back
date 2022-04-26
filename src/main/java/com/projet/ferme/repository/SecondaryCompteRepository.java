8d431918effcb14c0a09dae8399813ff00e1d8f0
DJUM <djum25@gmail.com>
2022-04-26 10:37:09 +0000

    Add method to find by primary compte

--- src/main/java/com/projet/ferme/repository/SecondaryCompteRepository.java
+++ src/main/java/com/projet/ferme/repository/SecondaryCompteRepository.java
@@ -1,9 +1,13 @@
 package com.projet.ferme.repository;
 
+import java.util.List;
+
 import com.projet.ferme.entity.SecondaryCompte;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SecondaryCompteRepository extends JpaRepository<SecondaryCompte, Long>{
+
+    List<SecondaryCompte> findByPrimary_id(Long id);
     
 }
