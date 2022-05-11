1ce46b2ca17ba6eaa3f93b6ec8f74c713b061a2a
DJUM <djum25@gmail.com>
2022-05-11 11:47:27 +0000

    Create compte and categoryCompte controller

---
+++ src/main/java/com/projet/ferme/entity/CategoryCompte.java
@@ -0,0 +1,43 @@
+package com.projet.ferme.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tbl_category_compte")
+public class CategoryCompte extends TimeModel{
+    
+    private String name;
+
+    private boolean debitAccount;
+	
+	@ManyToOne
+	@JoinColumn(name="parent_id", nullable = true)
+	private CategoryCompte parent;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CategoryCompte getParent() {
+        return this.parent;
+    }
+
+    public void setParent(CategoryCompte parent) {
+        this.parent = parent;
+    }
+
+    public boolean isDebitAccount() {
+        return debitAccount;
+    }
+
+    public void setDebitAccount(boolean debitAccount) {
+        this.debitAccount = debitAccount;
+    }
+}
