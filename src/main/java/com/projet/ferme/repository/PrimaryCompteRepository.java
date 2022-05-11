f36294e508d2c522a501a263c2a41ab551711237
DJUM <djum25@gmail.com>
2022-05-11 11:37:25 +0000

    Delete primary and secondary compte, replace by category compte

--- src/main/java/com/projet/ferme/repository/PrimaryCompteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projet.ferme.repository;
-
-import com.projet.ferme.entity.PrimaryCompte;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrimaryCompteRepository extends JpaRepository<PrimaryCompte, Long> {
-    
-}
