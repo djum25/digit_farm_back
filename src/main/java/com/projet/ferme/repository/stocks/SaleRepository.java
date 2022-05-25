67e5b374d150061ead251536300f720cd4c31e87
DJUM <djum25@gmail.com>
2022-05-25 13:13:30 +0000

    methode get sales by cashier

--- src/main/java/com/projet/ferme/repository/stocks/SaleRepository.java
+++ src/main/java/com/projet/ferme/repository/stocks/SaleRepository.java
@@ -1,5 +1,7 @@
 package com.projet.ferme.repository.stocks;
 
+import java.util.List;
+
 import com.projet.ferme.entity.stocks.Sale;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
 	Sale findBySubjectId(String subjectId);
+
+    List<Sale> findByCashier_id(Long id);
 
 }
