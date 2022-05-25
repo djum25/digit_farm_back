3269fb68d42e96b4abe965500c36c2d62c6c3832
DJUM <djum25@gmail.com>
2022-05-25 15:04:13 +0000

    test compenent anotation in UserAuthenticate

--- src/main/java/com/projet/ferme/entity/utils/UserAuthenticate.java
+++ src/main/java/com/projet/ferme/entity/utils/UserAuthenticate.java
@@ -6,7 +6,9 @@ import com.projet.ferme.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserAuthenticate {
 	
 	@Autowired
