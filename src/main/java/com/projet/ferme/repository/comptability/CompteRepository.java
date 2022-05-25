1171c0b2522736dbb4455da9a11d5c909528d44b
DJUM <djum25@gmail.com>
2022-05-25 11:08:39 +0000

    add method find by number

--- src/main/java/com/projet/ferme/repository/comptability/CompteRepository.java
+++ src/main/java/com/projet/ferme/repository/comptability/CompteRepository.java
@@ -1,9 +1,12 @@
 package com.projet.ferme.repository.comptability;
 
+import java.util.Optional;
+
 import com.projet.ferme.entity.comptability.Compte;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompteRepository extends JpaRepository<Compte, Long>{
     
+    Optional<Compte> findByNumber(String number);
 }
