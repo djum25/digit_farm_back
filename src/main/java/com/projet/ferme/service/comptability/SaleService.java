2f6ea209aa23c1cd4b27406eef6acd90c97883bc
DJUM <djum25@gmail.com>
2022-05-25 13:14:08 +0000

    make sales of cashier to counted true

--- src/main/java/com/projet/ferme/service/comptability/SaleService.java
+++ src/main/java/com/projet/ferme/service/comptability/SaleService.java
@@ -1,11 +1,14 @@
 package com.projet.ferme.service.comptability;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projet.ferme.entity.person.Cashier;
 import com.projet.ferme.entity.stocks.Sale;
 import com.projet.ferme.repository.stocks.SaleRepository;
 
@@ -36,5 +39,12 @@ public class SaleService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public int findSaleToCount(Cashier cashier){
+		List<Sale> sales = repository.findByCashier_id(cashier.getId());
+		sales = sales.stream().filter(s-> !s.isCounted()).
+		map(s-> {s.setCounted(true); return s;}).collect(Collectors.toList());
+		return repository.saveAll(sales).size();
 	}
 }
