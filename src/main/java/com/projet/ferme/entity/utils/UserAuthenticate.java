0c100b2db0e7acab881a67f2ba8ae13e56050ccd
DJUM <djum25@gmail.com>
2022-05-25 15:08:29 +0000

    change component to service

--- src/main/java/com/projet/ferme/entity/utils/UserAuthenticate.java
+++ src/main/java/com/projet/ferme/entity/utils/UserAuthenticate.java
@@ -6,9 +6,9 @@ import com.projet.ferme.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class UserAuthenticate {
 	
 	@Autowired
