c267da720d13c11851eb2dc200e2b7e4882e3942
DJUM <djum25@gmail.com>
2022-06-14 10:08:19 +0000

    add reimburse sale method

--- src/main/java/com/projet/ferme/service/comptability/SaleService.java
+++ src/main/java/com/projet/ferme/service/comptability/SaleService.java
@@ -3,6 +3,7 @@ package com.projet.ferme.service.comptability;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Service;
 
 import com.projet.ferme.entity.person.Cashier;
 import com.projet.ferme.entity.stocks.Sale;
+import com.projet.ferme.entity.utils.NewDate;
 import com.projet.ferme.repository.stocks.SaleRepository;
+import com.projet.ferme.service.utile.MapResponse;
 
 @Service
 public class SaleService {
@@ -46,5 +49,19 @@ public class SaleService {
 		sales = sales.stream().filter(s-> !s.isCounted()).
 		map(s-> {s.setCounted(true); return s;}).collect(Collectors.toList());
 		return repository.saveAll(sales).size();
+	}
+
+	public Map<String, Object> reimburseSale(Long id){
+		Optional<Sale> sale = repository.findById(id);
+		if (sale.isEmpty()) {
+			return new MapResponse().withSuccess(false).
+			withMessage("La vente n'est pas retrouvé").response();
+		} else {
+			sale.get().setReimburse(true);
+			sale.get().setUpdatedOn(new NewDate().getDate());
+			repository.save(sale.get());
+			return new MapResponse().withSuccess(true).withObject(sale.get()).
+			withMessage("Remboursé avec succé").response();
+		}
 	}
 }
